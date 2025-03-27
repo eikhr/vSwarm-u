8ef26447b46ef4ccc8001ee12f2e039db422fee0
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2025-03-27 13:07:15 +0100

    Configurable btb sizes

--- simulation/wkdir-tmpl/run_sim.tmpl.py
+++ simulation/wkdir-tmpl/run_sim.tmpl.py
@@ -40,7 +40,7 @@ print(ROOT)
 import sys
 sys.path.append(ROOT +'/gem5utils/systems/') # For the next line...
 from skylake.system import SklSystem
-from skylake.core import SklTunedCPU, InfBTBCPU, SplitBTBCPU, SingleBTBCPU
+from skylake.core import SklTunedCPU, InfBTBCPU, SplitBTBCPU, SingleBTBCPU, BranchPred
 
 import argparse
 def parse_arguments():
@@ -64,6 +64,8 @@ def parse_arguments():
                                 Evaluation mode: Will start from a previously taken checkpoint
                                 do some """)
     parser.add_argument("--btb", type = str, help = "Type of btb setup, 'inf', 'split' or 'single'")
+    parser.add_argument("--btb-user-entries", type = int, default=4096)
+    parser.add_argument("--btb-kernel-entries", type = int, default=4096)
     parser.add_argument("--take-checkpoints", action="store_true", default=False,
                         help="""Take a checkpoint after system is configured and ready
                             to start the proper simulation""")
@@ -287,10 +289,20 @@ if __name__ == "__m5_main__":
 
     kvm = True if args.mode == "setup" else False
 
-    cpuModel = InfBTBCPU if args.btb == "inf" else \
-          SplitBTBCPU if args.btb == "split" else \
-          SingleBTBCPU if args.btb == "single" else \
-          fatal("Invalid btb type")
+    if args.btb == "custom":
+        class CustomBTBBranchPred(BranchPred):
+            btbUser = SimpleBTB(numEntries=args.btb_user_entries, associativity=8, tagBits=12)
+            btbKernel = SimpleBTB(numEntries=args.btb_kernel_entries, associativity=8, tagBits=12)
+
+        class CustomBTBCPU(SklTunedCPU):
+            branchPred = CustomBTBBranchPred()
+
+        cpuModel = CustomBTBCPU
+    else:
+        cpuModel = InfBTBCPU if args.btb == "inf" else \
+              SplitBTBCPU if args.btb == "split" else \
+              SingleBTBCPU if args.btb == "single" else \
+              fatal("Invalid btb type")
 
     # create the system we are going to simulate
     system = SklSystem(args.kernel, args.disk, CPUModel=cpuModel, num_cpus=4, kvm=kvm)
